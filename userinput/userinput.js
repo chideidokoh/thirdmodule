f1964ea9d5773cf062b91b06fba7eb302b5a7b5e
Idokoh Divine <95508724+chideidokoh@users.noreply.github.com>
2022-03-24 02:02:00 +0100

    Challenge-one

---
+++ userinput/userinput.js
@@ -0,0 +1,7 @@
+var name = prompt ('What is your first name?');
+var middleName = prompt ('What is your middle name?');
+var lastName = prompt ('What is your last name?');
+
+document.write ('<h1>Hello '+ name + ' ' + middleName + ' ' + lastName +'</h1>');
+
+alert ('Hello '+ name + ' ' + middleName + ' ' + lastName);
