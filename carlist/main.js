3927d0dec50f906a4d0c38b91d060f59e1916354
Idokoh Divine <95508724+chideidokoh@users.noreply.github.com>
2022-03-28 01:49:18 +0100

    challenge-four

---
+++ carlist/main.js
@@ -0,0 +1,17 @@
+var cars = [ 'toyota','honda','mazda','mitsubishi','tesla','lexus'];
+
+console.log ('toyota');
+
+for (var i=0; i < cars.length; i++) {
+    console.log(cars[5]);
+}
+
+/*console.log ('lexus');*/
+
+cars[6] = 'mercedes-benz'
+console.log ('mercedes-benz');
+
+
+for (var i=0; i < cars.length; i++) {
+    console.log(cars[i]);
+}
